37e1535ff47bba9364639400d9e5f09414b8730e
Jose Claudio <xoceklawdyo@hotmail.com>
2017-08-07 15:49:43 -0500

    Practicas Led

---
+++ Practica3/Untitled2/Untitled2.ino
@@ -0,0 +1,10 @@
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+}
